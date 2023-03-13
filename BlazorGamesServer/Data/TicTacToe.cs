725aaed20cce0abce0e943a23c39c231f6bb53ed
cpgoodwi <cpgoodwi@asu.edu>
2023-03-13 15:36:14 -0700

    Add project files.

---
+++ BlazorGamesServer/Data/TicTacToe.cs
@@ -0,0 +1,7 @@
+﻿namespace BlazorGamesServer.Data
+{
+    public class TicTacToe
+    {
+        char[,] Board = new char[3, 3];
+    }
+}
---
+++ BlazorGamesServer/Data/TicTacToeService.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorGamesServer.Data
+{
+    public class TicTacToeService
+    {
+        char[,] Board = new char[3, 3];
+
+        public Task GetBoardAsync()
+        {
+            return Task.FromResult(Board);
+        }
+    }
+}
